2a32f9cc4802fc2282fc52ea7bf68d88513b7a9d
Raouf <66827927+Raouf217@users.noreply.github.com>
2022-06-07 17:44:58 +0200

    Time: 60 ms (72.95%), Space: 14.1 MB (94.99%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -5,20 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # values = {}
-        # for idx, value in enumerate(nums):
-        #     if target - value in values:
-        #         return [values[target - value], idx]
-        #     else:
-        #         values[value] = idx
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[j] == target - nums[i]:
-        #             return [i, j]
-        hashmap = {}
-        for i in range(len(nums)):
-            hashmap[nums[i]] = i
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap and hashmap[complement] != i:
-                return [i, hashmap[complement]] 
+        values = {}
+        for idx, value in enumerate(nums):
+            if target - value in values:
+                return [values[target - value], idx]
+            values[value] = idx
+        
