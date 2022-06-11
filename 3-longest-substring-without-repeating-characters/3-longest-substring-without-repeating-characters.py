3002932c2bc033108d9f6105757de4b636289943
Raouf <66827927+Raouf217@users.noreply.github.com>
2022-06-11 21:37:30 +0200

    Update 3-longest-substring-without-repeating-characters.py

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,21 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # i =0
-        # j = 0
-        # d={}
-        # ans = 0
-        # while j < len(s):
-        #     if s[j] not in d or i>d[s[j]]:
-        #         ans = max(ans,(j-i+1))
-        #         d[s[j]] = j
-        #     else:
-        #         i = d[s[j]]+1
-        #         ans = max(ans,(j-i+1))
-        #         j-=1
-        #     #print(ans)
-        #     j+=1
-        # return ans
+       
         last_idx = {}
         max_len = 0
         start_idx = 0
