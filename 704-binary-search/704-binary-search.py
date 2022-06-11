06921e52d630503d7259f80f8de5d893a568867e
Raouf <66827927+Raouf217@users.noreply.github.com>
2022-06-12 00:39:55 +0200

    Time: 224 ms (62.97%), Space: 14.5 MB (94.40%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,9 +1,30 @@
 class Solution(object):
+    def binary(self,nums, low, high,target):
+        if high >=low:
+            mid = (high + low) // 2
+ 
+            # If element is present at the middle itself
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                return self.binary(nums, low, mid - 1, target)
+
+            # Else the element can only be present in right subarray
+            else:
+                return self.binary(nums, mid + 1, high, target)
+
+        else:
+            return -1
+    
     def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        if target not in nums : return -1
-        return nums.index(target)
+        x =  self.binary(nums,0,len(nums)-1,target)
+        return x
+       
+        # if target not in nums : return -1
+        # return nums.index(target)
+    
