68a7ae9d555cff751dc6f361485cfa5ca3cdf046
Raouf <66827927+Raouf217@users.noreply.github.com>
2022-06-15 21:16:01 +0200

    Time: 824 ms (14.15%), Space: 15.5 MB (63.64%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,7 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if 0 in nums:
-            for i in nums:
-                nums.insert(len(nums),0)
-                nums.pop(nums.index(0))
+        
+#         for i in nums:
+#             nums.insert(len(nums),0)
+#             nums.pop(nums.index(0))
+        for j in range(nums.count(0)):
+              nums.remove(0)
+              nums.append(0)
