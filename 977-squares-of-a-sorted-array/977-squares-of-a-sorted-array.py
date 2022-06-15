4679406224bace82b0e72e9110f258c9f3279758
Raouf <66827927+Raouf217@users.noreply.github.com>
2022-06-15 19:01:09 +0200

    Time: 834 ms (5.02%), Space: 16.5 MB (5.09%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -0,0 +1,26 @@
+def quicksort(array):
+    
+    if len(array) < 2:
+        return array
+
+    low, same, high = [], [], []
+
+    
+    pivot = array[randint(0, len(array) - 1)]
+
+    for item in array:
+        
+        if item < pivot:
+            low.append(item)
+        elif item == pivot:
+            same.append(item)
+        elif item > pivot:
+            high.append(item)
+
+    
+    return quicksort(low) + same + quicksort(high)
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ls = [x*x for x in nums]
+        return quicksort(ls)
