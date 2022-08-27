cd50cd64e1f436dc5094bbed162fa87f5192c79d
Raouf <66827927+Raouf217@users.noreply.github.com>
2022-08-27 03:40:19 +0200

    Time: 34 ms (86.32%), Space: 13.9 MB (57.48%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,17 +1,19 @@
-from functools import cache
+# from functools import cache
 
 class Solution:
     
-    @cache # cache for memoization
-    def climbStairs(self, n: int) -> int:
+    # @cache # cache for memoization
+    def climbStairs(self, n: int, memo = None) -> int:
         # if we have 1 step left we can only climb that with one step
         # if we have 2 steps we can climb them in two ways (see example 1 in question)
         if n < 3:
             return n
-        
-        ways = self.climbStairs(n-1) + self.climbStairs(n-2)
+        if memo is None :
+            memo = {}
+        if n in memo: return memo[n]
+        memo[n] = self.climbStairs(n-1,memo) + self.climbStairs(n-2,memo)
         # at each step X we can either take 1 to 2 steps leaving the 
         # remaining number of steps to be either X - 1 or X - 2
         # depending on our choice
 
-        return ways
+        return memo[n]
